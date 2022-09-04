ae1ebdf1999083a01453c70f70dd376176cc6b57
Mink-a <rahulakhant@gmail.com>
2022-09-04 13:12:55 +0630

    pnpm run

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ dotenv.config();
 // const connection = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
-//   password: "root",
-//   database: "note_app",
+//   password: "",
+//   database: "notes_db",
 // });
 
 // connection.query("SELECT * FROM `NOTES`", function (err, result, field) {
@@ -17,10 +17,10 @@ dotenv.config();
 
 const pool = mysql
   .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "notes_db",
   })
   .promise();
 
@@ -65,4 +65,4 @@ export async function deleteNote(id) {
 // const allNotes = await getNotes();
 // const singleNote = await getNoteById(1);
 // const insertNote = await createNote("FOURTH NOTE", "MAY THE 4TH BE WITH YOU!");
-// console.log(insertNote);
+// console.log(allNotes);
