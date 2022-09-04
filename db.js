bce39322936dfd9d3a6482936f37cfd25e057774
Mink-a <rahulakhant@gmail.com>
2022-09-04 13:43:02 +0630

    code format Tables

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ dotenv.config();
 //   database: "notes_db",
 // });
 
-// connection.query("SELECT * FROM `NOTES`", function (err, result, field) {
+// connection.query("SELECT * FROM `Notes`", function (err, result, field) {
 //   console.log(err);
 //   console.log(result);
 // });
@@ -25,19 +25,19 @@ const pool = mysql
   .promise();
 
 export async function getNotes() {
-  const [row] = await pool.query("SELECT * FROM NOTES");
+  const [row] = await pool.query("SELECT * FROM Notes");
   return row;
 }
 
 export async function getNoteById(id) {
-  const [row] = await pool.query(`SELECT * FROM NOTES WHERE ID=?`, [id]);
+  const [row] = await pool.query(`SELECT * FROM Notes WHERE id=?`, [id]);
   return row;
 }
 
 export async function createNote(title, content) {
   const [result] = await pool.query(
     `
-  INSERT INTO NOTES (TITLE, CONTENTS) VALUES
+  INSERT INTO Notes (title, contents) VALUES
   (?,?)
   `,
     [title, content]
@@ -49,18 +49,18 @@ export async function createNote(title, content) {
 export async function updateNote(id, title, content) {
   const [result] = await pool.query(
     `
-    UPDATE NOTES SET TITLE = ?, CONTENTS =? WHERE ID=?`,
+    UPDATE Notes SET title = ?, contents =? WHERE id=?`,
     [title, content, id]
   );
   return getNoteById(id);
 }
 
 export async function deleteNote(id) {
-  const [result] = await pool.query(`DELETE FROM NOTES WHERE ID=?`, [id]);
+  const [result] = await pool.query(`DELETE FROM Notes WHERE id=?`, [id]);
   return result;
 }
 
-// console.log(await updateNote(2, "EDITED TITLE", "EDITED CONTENTS"));
+// console.log(await updateNote(2, "EDITED title", "EDITED contents"));
 
 // const allNotes = await getNotes();
 // const singleNote = await getNoteById(1);
